05f0c407eee746fda100070dc106f57331f0bed0
miguel garcia <miguel.gareva@gmail.com>
2024-12-04 12:49:51 +0100

    methods created for class Employee and intance of employee created and called the method calculateIfTaxes in index.php

--- 1.4.1_POO1_nivell1/Employee.php
+++ 1.4.1_POO1_nivell1/Employee.php
@@ -2,9 +2,22 @@
 declare(strict_types=1);
 class Employee{
     public string $name;
-    public float $salari;
+    public float $salary;
 
-    
+    public function __construct(string $name, float $salary){
+
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    public function calculateIfTaxes() {
+
+        if($this->salary > 6000) {
+            echo $this->name . " must pay taxes.";
+        } else {
+            echo $this->name . " does not have to pay taxes.";
+        }
+    }
 }
 
 ?>
--- 1.4.1_POO1_nivell1/index.php
+++ 1.4.1_POO1_nivell1/index.php
@@ -2,4 +2,8 @@
 
 require_once("Employee.php");
 
+$employee = new Employee ("Miguel", 120000);
+
+$employee->calculateIfTaxes();
+
 ?>
