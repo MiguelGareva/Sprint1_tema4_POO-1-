668a150a78c73a47509ce01c42b679cee9dcca91
miguel garcia <miguel.gareva@gmail.com>
2024-12-18 14:19:30 +0100

    logic for print dices and shapes implemented

--- 1.4.1_POO1_nivell2/index.php
+++ 1.4.1_POO1_nivell2/index.php
@@ -3,8 +3,12 @@
 declare(strict_types=1);
 require_once "PokerDice.php";
 
-$dice = [];
+$dices = [];
 for ($i=0; $i < 5; $i++) { 
-    $dice = new PokerDice();
+    $dices = new PokerDice();
+}
+
+foreach ($dices as $index => $dice) {
+    # code...
 }
 ?>
