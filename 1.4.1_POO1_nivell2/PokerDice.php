14b0485b85c8bd646940d00cda12c10c39617196
miguel garcia <miguel.gareva@gmail.com>
2024-12-18 13:21:21 +0100

    atributes of class PokerDice, setted

--- 1.4.1_POO1_nivell2/PokerDice.php
+++ 1.4.1_POO1_nivell2/PokerDice.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 class PokerDice {
-    
+
+    private const SHAPES = ['A','K','Q','J','8','7'];
+    private static int $totalThrows = 0;
+    private string $lastShape;
 }
 ?>
