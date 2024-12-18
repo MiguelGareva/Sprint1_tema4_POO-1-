8ac9aa4fb56a3da8e817cbe5cfd468ff07075915
miguel garcia <miguel.gareva@gmail.com>
2024-12-18 13:37:34 +0100

    function throwDice implemented

--- 1.4.1_POO1_nivell2/PokerDice.php
+++ 1.4.1_POO1_nivell2/PokerDice.php
@@ -7,5 +7,10 @@ class PokerDice {
     private const SHAPES = ['A','K','Q','J','8','7'];
     private static int $totalThrows = 0;
     private string $lastShape;
+
+    public function trhowDice():void{
+        $this->lastShape = self::SHAPES[random_int(0, count(self::SHAPES) - 1)];
+        self::$totalThrows++;
+    }
 }
 ?>
