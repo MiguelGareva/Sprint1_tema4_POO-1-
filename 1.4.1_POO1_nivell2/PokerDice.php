3efffe75a1292cc707fc0b2ae90381d5bdd02ba3
miguel garcia <miguel.gareva@gmail.com>
2024-12-18 13:45:10 +0100

    function nameShape implemented

--- 1.4.1_POO1_nivell2/PokerDice.php
+++ 1.4.1_POO1_nivell2/PokerDice.php
@@ -6,11 +6,17 @@ class PokerDice {
 
     private const SHAPES = ['A','K','Q','J','8','7'];
     private static int $totalThrows = 0;
-    private string $lastShape;
+    private ?string $lastShape = null;
 
     public function trhowDice():void{
         $this->lastShape = self::SHAPES[random_int(0, count(self::SHAPES) - 1)];
         self::$totalThrows++;
     }
+
+    public function nameShape(): ?string{
+        return $this->lastShape;
+    }
+
+    
 }
 ?>
