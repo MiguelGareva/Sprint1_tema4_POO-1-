0b7285c2a7b5551eb2d986ef075a049f9b216b97
miguel garcia <miguel.gareva@gmail.com>
2024-12-18 14:26:04 +0100

    problem solved on line 8 of index.php and getTotalThrows used once

--- 1.4.1_POO1_nivell2/index.php
+++ 1.4.1_POO1_nivell2/index.php
@@ -5,10 +5,13 @@ require_once "PokerDice.php";
 
 $dices = [];
 for ($i=0; $i < 5; $i++) { 
-    $dices = new PokerDice();
+    $dices[] = new PokerDice();
 }
 
 foreach ($dices as $index => $dice) {
-    # code...
+    $dice->trhowDice();
+    echo "Dado nº" . ($index + 1) . ": " . $dice->nameShape() . "<br>";
 }
+
+echo "Nº total de tiradas: " . PokerDice::getTotalThrows() . "<br>";
 ?>
