af83a8527e94209a77164112ae292a1c61077c48
miguel garcia <miguel.gareva@gmail.com>
2024-12-19 13:02:59 +0100

    classes atributes added

--- 1.4.1_POO_nivell3/Cine.php
+++ 1.4.1_POO_nivell3/Cine.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php 
+declare(strict_types=1);
+require_once "Pelicula.php";
+class Cine{
+    public string $nombre;
+    public string $localidad;
+    public array $cartelera;
+
+}
+?>
--- 1.4.1_POO_nivell3/Pelicula.php
+++ 1.4.1_POO_nivell3/Pelicula.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php 
+declare(strict_types=1);
+
+class Pelicula{
+    public string $titulo;
+    public int $duracion;
+    public string $director;
+}
+
+?>
