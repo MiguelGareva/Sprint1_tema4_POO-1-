3c5f814689d44f70b20d36e665dd197e5add2e25
miguel garcia <miguel.gareva@gmail.com>
2025-01-15 13:59:32 +0100

    first cinemas and movies instances on index.php

--- 1.4.1_POO_nivell3/index.php
+++ 1.4.1_POO_nivell3/index.php
@@ -1,1 +1,13 @@
-<?php ?>
+<?php 
+declare(strict_types=1);
+require_once "Cine.php";
+
+$cine1 = new Cine("Cines Verdi","Barcelona");
+
+$pelicula1 = new Pelicula("Amanece que no es poco",110,"Jose Luis Cuerda");
+$pelicula2 = new Pelicula("El café de la Marina", 94, "Domingo Pruna");
+
+$cine1->agregarPelicula($pelicula1);
+$cine1->agregarPelicula($pelicula2);
+$cine1->mostrarDatosCartelera();
+?>
