0c097184fb888244e1c26f34b44501af0ff46564
miguel garcia <miguel.gareva@gmail.com>
2025-01-15 13:10:24 +0100

    construct methods created

--- 1.4.1_POO_nivell3/Cine.php
+++ 1.4.1_POO_nivell3/Cine.php
@@ -4,7 +4,11 @@ require_once "Pelicula.php";
 class Cine{
     public string $nombre;
     public string $localidad;
-    public array $cartelera;
+    public array $cartelera = [];
+
+    public function __construct(string $nombre, string $localidad, array $cartelera){
+
+    }
 
 }
 ?>
--- 1.4.1_POO_nivell3/Pelicula.php
+++ 1.4.1_POO_nivell3/Pelicula.php
@@ -5,6 +5,12 @@ class Pelicula{
     public string $titulo;
     public int $duracion;
     public string $director;
+
+    public function __construct(string $titulo, int $duracion, string $director){
+        $this->titulo = $titulo;
+        $this->duracion = $duracion;
+        $this->director = $director;
+    }
 }
 
 ?>
